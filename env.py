dc5109ee637477058c2947007bc967397aca3d9d
MikeMaroni9 <k.vaitka@gmail.com>
2024-02-03 20:38:09 +0000

    removed env file from the front end, I guess I accidentaly have added it there

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'cloudinary://827227132985194:1YpVoPKEK9R2tnxZnbN5daiIzlg@dyv1fobjp'
-os.environ['SECRET_KEY'] = "asdsdfzsaw45t34tdASd1223e"
-os.environ['DEV'] = '1'
-os.environ["DATABASE_URL"] = "postgres://broeygun:sPBIU4bv-0RjadqXbBNKk3lE2DFX7wEX@surus.db.elephantsql.com/broeygun"
